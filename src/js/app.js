5a09026efb103859615b1d22eca27fef9ff9bac3
Sheremet1 <eparis@atlassian.com>
2023-09-28 23:24:30 +0300

    init5

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import Field from './field';
-import Game from './game';
-
-const fieldElem = document.querySelector('.field');
-const scoreElem = document.querySelector('.score');
-
-const game = new Game(fieldElem, scoreElem);
-setInterval(game.move.bind(game), 2000);
